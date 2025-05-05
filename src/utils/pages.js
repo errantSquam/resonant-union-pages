6bc5b0fc4599d993f36298cab67874d565d7a8dd
KimmyKitsuragi <bioteamaq@gmail.com>
2025-05-05 22:21:50 +0800

    add barebones navbar

---
+++ src/utils/pages.js
@@ -0,0 +1,14 @@
+export const pages = [
+    {
+        label: "Home",
+        path: ""
+    },
+    {
+        label: "Albums",
+        path: "albums"
+    },
+    {
+        label: "Members",
+        path: "members"
+    }
+]
