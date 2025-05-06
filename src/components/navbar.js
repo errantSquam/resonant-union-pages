c28bb5f415951a7696c432cff9ef3d3d6ff4ece2
Errant <eltehhh@gmail.com>
2025-05-06 22:51:42 +0800

    hover

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,8 +5,11 @@ import { Link } from "react-router-dom";
 
 const JrpgGradientBox = ({children}) => {
 
-    //todo: add highlight on hover, probably 
-    return <div className="text-xl font-pixelify bg-gradient-to-b from-blue-700 from-20% to-slate-950 to-90% 
+    //background cant do transition... gradients are bgs.... L...
+
+    return <div className="text-xl font-pixelify 
+    hover:from-blue-500 hover:to-blue-700 
+    bg-gradient-to-b from-blue-700 from-20% to-slate-950 to-90% 
     px-2 py-0.5 border border-2 border-white rounded-md uppercase">
         {children}
         </div>
