896a6aa754b1e105de8772f4585cdd80bee47fc4
Errant <eltehhh@gmail.com>
2025-05-06 20:51:39 +0800

    some front page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,14 @@
 import { pages } from "../utils/pages"
 import { Link } from "react-router-dom";
 
+
 export default function Navbar() {
     return <div className="w-full">
         <div className="flex flex-row items-center justify-end mx-16 my-6 gap-4">
             {pages.map((page, index) => {
-                return <div className="text-2xl"><Link to={page.path}>{page.label}</Link></div>
+                return <div className="text-xl"><Link to={page.path}>{page.label}</Link></div>
             })}
-            <img className="w-[20px] h-[20px]" src=""/>
+            <img className="w-[20px] h-[20px]" src="RU.png"/>
         </div>
     </div>
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,18 @@
 const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
+  return <div className="bg-slate-950 min-h-screen">
+    <div className="px-20 py-4">
+
+      <div className = "font-[dm-serif-display-regular]">
+        <b className="text-white text-xl">We are</b>
+        <div className="text-7xl text-orange-500">
+          <b><span className="text-amber-500">R</span>ESONANT</b>
+          <br/>
+          <b><span className="text-amber-500">U</span>NION</b>
+        </div>
+      </div>
+
+    </div>
+  </div>;
+};
+
 export default Home;
-  
