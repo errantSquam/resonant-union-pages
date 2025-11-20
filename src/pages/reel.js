b19a4c0ec858e02893487159fd672d5b3039c28e
Errant <48056361+errantSquam@users.noreply.github.com>
2025-11-20 14:22:03 +0800

    i accidentally removed async let me put that back

--- src/pages/reel.js
+++ src/pages/reel.js
@@ -8,6 +8,6 @@ export const Reel = () => {
     return <div className="w-full py-5 px-20">
         {isLoading && <div className = "text-white font-pixelify">Loading...</div>}
         <div id="85b7cd5d-4055-47e0-8d6e-a4f2ca44be89"><script src="https://sharefol.io/api/player/85b7cd5d-4055-47e0-8d6e-a4f2ca44be89"
-             type="text/javascript" id = "reel" onLoad={() => { setLoading(false) }}></script></div>
+             type="text/javascript" async id = "reel" onLoad={() => { setLoading(false) }}></script></div>
     </div>
 }
