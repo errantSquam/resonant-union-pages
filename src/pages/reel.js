19d0909fe40e88ab1e8380466391367ad58ba2f5
Errant <48056361+errantSquam@users.noreply.github.com>
2025-11-20 14:28:34 +0800

    ok nvm any onLoad script just breaks the reel what the hell

--- src/pages/reel.js
+++ src/pages/reel.js
@@ -6,8 +6,10 @@ export const Reel = () => {
 
 
     return <div className="w-full py-5 px-20">
-        {isLoading && <div className = "text-white font-pixelify">Loading...</div>}
-        <div id="85b7cd5d-4055-47e0-8d6e-a4f2ca44be89"><script src="https://sharefol.io/api/player/85b7cd5d-4055-47e0-8d6e-a4f2ca44be89"
-             type="text/javascript" async id = "reel" onLoad={() => { setLoading(false) }}></script></div>
+        <div id="85b7cd5d-4055-47e0-8d6e-a4f2ca44be89">
+            <script src="https://sharefol.io/api/player/85b7cd5d-4055-47e0-8d6e-a4f2ca44be89"
+             async type="text/javascript"
+             ></script>
+        </div>
     </div>
 }
